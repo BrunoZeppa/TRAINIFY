eb64d531c4020af0adfc6061a84653f41d771b6e
brunoz <zegb94@gmail.com>
2022-06-19 03:34:23 -0500

    update dasboard

--- assets/js/dashbord-mobile.js
+++ assets/js/dashbord-mobile.js
@@ -1,13 +1,54 @@
 
 'use strict'
 
+let contenedor = document.querySelector( ".dashbord-de-usuarios" );
+let fragmento = new DocumentFragment;
 
 const users = [
     {
-        id: 0,
-        nombre: "Fulanito Perez",
-        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sed id qui delectus possimus vitae.",
-        imgUrl: "https://picsum.photos/id/1005/200/300"
+        id: "BrunoBZG24",
+        selfie: "https://picsum.photos/id/1005/200/300",
+        nombre: "Bruno Zeppa",
+        fechaDeNacimiento: 24/02/1994,
+        correo: "zegb94@gmail.com",
+        suscripcion: "estandar",
+        tas: 120, 
+        tad: 80,
+        fcr: 60,
+        peso: 73,
+        altura: 168,
+        cCintura: 80,
+        cCadera: 100,
+        cBrazo: 37,
+        cPantorrilla: 40,
+        pTriceps: 14,
+        pSubEscapular: 20,
+        pSupraIliaco: 14,
+        pAbdominal: 23,
+        pCuadricipital: 12,
+        pComodin: 18, 
+        pPantorrilla: 12,
+        nivHombros: 10,
+        escoliosis: 5,
+        nivCadera: 5,
+        aliTobillos: 10,
+        sifosis: 10,
+        lordosis: 10,
+        flexibilidad: -4,
+        angSalto: 45, 
+        fc1: 70,
+        fc2: 117,
+        fc3: 90,
+        fcVo2max: 169,
+        cuads: 15, 
+        isquios: 13,
+        gluteos: 12,
+        pecho: 19,
+        remo: 20,
+        hombro: 20, 
+        core: 10,
+        espaldaBaja:12,
+        fecha: 01/05/2022
     },
     {
         id: 1,
@@ -28,10 +69,6 @@ const users = [
         imgUrl: "https://picsum.photos/id/342/200/300"
     },
 ]
-
-let contenedor = document.querySelector( ".dashbord-de-usuarios" )
-
-let fragmento = new DocumentFragment
 
 for (let i = 0; i < users.length; i++) {
 
@@ -55,3 +92,4 @@ for (let i = 0; i < users.length; i++) {
 }
 
 console.log(contenedor.appendChild( fragmento ))
+
