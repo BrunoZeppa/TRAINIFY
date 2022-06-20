b9d295d541b44a044305c5b5a5483fdcaff40231
brunoz <zegb94@gmail.com>
2022-06-19 23:45:13 -0500

    consumo de api existoso

--- assets/js/dashbord-mobile.js
+++ assets/js/dashbord-mobile.js
@@ -1,18 +1,25 @@
 
 'use strict'
 
-let contenedor = document.querySelector( ".dashbord-de-usuarios" );
-let fragmento = new DocumentFragment;
+document.addEventListener("DOMContentLoaded", () => {
+    filtrarUsuarios("BrunoBZG24")
+
+})
+
+let dashboardID = document.querySelector(".dashbord-de-usuarios");
+
+
 
 const users = [
     {
         id: "BrunoBZG24",
         selfie: "https://picsum.photos/id/1005/200/300",
         nombre: "Bruno Zeppa",
-        fechaDeNacimiento: 24/02/1994,
+        edad: 28,
         correo: "zegb94@gmail.com",
         suscripcion: "estandar",
-        tas: 120, 
+        genero: "masculino",
+        tas: 120,
         tad: 80,
         fcr: 60,
         peso: 73,
@@ -26,7 +33,7 @@ const users = [
         pSupraIliaco: 14,
         pAbdominal: 23,
         pCuadricipital: 12,
-        pComodin: 18, 
+        pComodin: 18,
         pPantorrilla: 12,
         nivHombros: 10,
         escoliosis: 5,
@@ -35,26 +42,66 @@ const users = [
         sifosis: 10,
         lordosis: 10,
         flexibilidad: -4,
-        angSalto: 45, 
+        angSalto: 45,
         fc1: 70,
         fc2: 117,
         fc3: 90,
         fcVo2max: 169,
-        cuads: 15, 
+        cuads: 15,
         isquios: 13,
         gluteos: 12,
         pecho: 19,
         remo: 20,
-        hombro: 20, 
+        hombro: 20,
         core: 10,
-        espaldaBaja:12,
-        fecha: 01/05/2022
+        espaldaBaja: 12,
+        fecha: "20 Marzo"
     },
     {
-        id: 1,
-        nombre: "Carlitos Sanchez",
-        descripcion: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum sed id qui delectus possimus vitae.",
-        imgUrl: "https://picsum.photos/id/1012/200/300"
+        id: "DeyaniraDRS22",
+        selfie: "https://picsum.photos/200/300",
+        nombre: "Deyanira Rossette",
+        edad: 38,
+        correo: "dsamedi@gmail.com",
+        suscripcion: "estandar",
+        genero: "femenino",
+        tas: 120,
+        tad: 80,
+        fcr: 60,
+        peso: 73,
+        altura: 168,
+        cCintura: 80,
+        cCadera: 100,
+        cBrazo: 37,
+        cPantorrilla: 40,
+        pTriceps: 14,
+        pSubEscapular: 20,
+        pSupraIliaco: 14,
+        pAbdominal: 23,
+        pCuadricipital: 12,
+        pComodin: 18,
+        pPantorrilla: 12,
+        nivHombros: 10,
+        escoliosis: 5,
+        nivCadera: 5,
+        aliTobillos: 10,
+        sifosis: 10,
+        lordosis: 10,
+        flexibilidad: -4,
+        angSalto: 45,
+        fc1: 70,
+        fc2: 117,
+        fc3: 90,
+        fcVo2max: 169,
+        cuads: 15,
+        isquios: 13,
+        gluteos: 12,
+        pecho: 19,
+        remo: 20,
+        hombro: 20,
+        core: 10,
+        espaldaBaja: 12,
+        fecha: "20 ABRIL"
     },
     {
         id: 2,
@@ -69,27 +116,42 @@ const users = [
         imgUrl: "https://picsum.photos/id/342/200/300"
     },
 ]
+function filtrarUsuarios(id) {
+    let fragmento = "";
+    let usuarioFiltrados = users.filter((users) => users.id == id);
+    usuarioFiltrados.forEach((users) => {
+        fragmento += `
+        <div class="card-user">
+          <div>
+              <img src="${users.selfie}" alt="">
+              <h2><span class="id">id</span>: ${users.id}</h2>
+          </div>
+          <div>
+              <p> <span>edad</span>:${users.edad} años</p>
+              <p> <span>genero</span>:${users.genero}</p>
+              <p> <span>correo registrado</span>:${users.correo}</p>
+              <p> <span>suscripcion</span>:${users.suscripcion}</p>
+              <a href="#" class="button">cambiar</a>
+              <br><br>
+          </div>
+      </div>`;
+    });
 
-for (let i = 0; i < users.length; i++) {
+    dashboardID.innerHTML = fragmento;
+};
 
-    let div = document.createElement("div")
-    div.classList.add( "tarjetas-usuario" )
-
-    let img = document.createElement("img")
-    img.src = users[i].imgUrl
-    
-    let h1 = document.createElement("h1")
-    h1.textContent = users[i].nombre
-    
-    let p = document.createElement("p")
-    p.textContent = users[i].descripcion
-    
-    div.appendChild(img)
-    div.appendChild(h1)
-    div.appendChild(p)
-    
-    fragmento.appendChild( div )
+function porcentajeGrasa( genero, edad){
+    if(genero == "masculino" && edad <= 30){
+        ((users.pTriceps + users.pSubEscapular + users.pSupraIliaco + users.pAbdominal + users.pCuadricipital + users.pComodin) * 0.097)+3.64
+    }
+    if(genero == "masculino" && edad >= 31){
+        ((users.pTriceps + users.pSubEscapular + users.pSupraIliaco + users.pAbdominal + users.pCuadricipital + users.pComodin) * 0.1066)+4.975
+    }
+    if(genero == "femenino" && edad <= 30){
+        ((users.pTriceps + users.pSubEscapular + users.pSupraIliaco + users.pAbdominal + users.pCuadricipital + users.pComodin) * 0.217)-4.47
+    }
+    if(genero == "masculino" && edad >= 31){
+        ((users.pTriceps + users.pSubEscapular + users.pSupraIliaco + users.pAbdominal + users.pCuadricipital + users.pComodin) * 0.224)-2.8
+    }
+    return porcentajeGrasa("masculino",28)
 }
-
-console.log(contenedor.appendChild( fragmento ))
-
